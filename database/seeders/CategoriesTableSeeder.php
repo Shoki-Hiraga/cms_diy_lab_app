d246232e9702eea1cac6a4be0279d9d7d6635b6c
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-10-12 03:49:28 +0900

    seeder作成

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -2,16 +2,47 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $categories = [
+            'ガーデニング',
+            '屋上・ベランダ',
+            '庭づくり',
+            '車',
+            'バイク',
+            '自転車',
+            '単管パイプ',
+            '家具制作',
+            'ホームインテリア',
+            '照明',
+            'リフォーム',
+            'リペア',
+            '電子工作',
+            '子どものおもちゃ',
+            'ペット',
+            '100均DIY',
+            'キャンプ・アウトドア',
+            '塗装',
+            'リサイクル',
+            '収納',
+            '壁・床',
+            '木工',
+            '溶接・金属加工',
+            '編み物・手芸',
+            'ガジェット',
+        ];
+
+        foreach ($categories as $name) {
+            DB::table('categories')->insert([
+                'name' => $name,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
--- database/seeders/ToolsTableSeeder.php
+++ database/seeders/ToolsTableSeeder.php
@@ -2,16 +2,44 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ToolsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $tools = [
+            'サンダー',
+            '溶接機',
+            'ノコギリ・ジグソー',
+            'スプレーガン',
+            'インパクトレンチ',
+            'ドライバー',
+            'ペンチ・プライヤー',
+            'ハンマー・バール',
+            'メジャー・定規・さしがね',
+            'カッターナイフ・ハサミ',
+            'ヤスリ・サンドペーパー',
+            '水平器',
+            'レンチ・スパナ',
+            '電動ドリル・インパクトドライバー',
+            'ルーター・トリマー',
+            '高圧洗浄機',
+            'ヒートガン',
+            'コンベックス',
+            'クランプ・バイス',
+            '鉋(かんな)',
+            '鑿(のみ)',
+            '錐(きり)',
+        ];
+
+        foreach ($tools as $name) {
+            DB::table('tools')->insert([
+                'name' => $name,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
